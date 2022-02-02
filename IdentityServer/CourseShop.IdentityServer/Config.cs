afb10a4f268f1550e8e8787f6c971253409f328b
Raşit Şahin <mrsahinrasit@gmail.com>
2022-02-03 02:33:24 +0300

    IdentityServer Added

---
+++ IdentityServer/CourseShop.IdentityServer/Config.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CourseShop.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                 new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+                 new ApiResource("photo_stock_catalog"){Scopes={"photo_stock_fullpermission"}},
+                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName),
+            };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                 new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){ Name="roles", DisplayName="Roles", Description="Kullanıcı rolleri", UserClaims=new []{ "role"} }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("catalog_fullpermission","Catalog Api için full erişim"),
+                new ApiScope("photo_stock_fullpermission","Photo Stock Api için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "WebMvcClient",
+                    ClientName = "WebMvcClient Project",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "catalog_fullpermission" , "photo_stock_fullpermission" , IdentityServerConstants.LocalApi.ScopeName}
+                },
+                new Client
+                {
+                    ClientName="Asp.Net Core MVC",
+                    ClientId="WebMvcClientForUser",
+                    AllowOfflineAccess=true,
+                    ClientSecrets= {new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
+                    AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={ "basket_fullpermission"
+                                    ,"order_fullpermission"
+                                    ,"gateway_fullpermission"
+                                    ,IdentityServerConstants.StandardScopes.Email
+                                    ,IdentityServerConstants.StandardScopes.OpenId
+                                    ,IdentityServerConstants.StandardScopes.Profile
+                                    ,IdentityServerConstants.StandardScopes.OfflineAccess
+                                    ,IdentityServerConstants.LocalApi.ScopeName
+                                    ,"roles" 
+                    },
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration=TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage= TokenUsage.ReUse
+                },
+                new Client
+                {
+                    ClientName="Token Exchange Client",
+                    ClientId="TokenExhangeClient",
+                    ClientSecrets= {new Secret("secret".Sha256())},
+                    AllowedGrantTypes= new []{ "urn:ietf:params:oauth:grant-type:token-exchange" },
+                    AllowedScopes={ "discount_fullpermission", "payment_fullpermission", IdentityServerConstants.StandardScopes.OpenId }
+                },
+            };
+    }
+}
--- Services/Catalog/CourseShop.Services.Catalog/Program.cs
+++ Services/Catalog/CourseShop.Services.Catalog/Program.cs
@@ -1,5 +1,7 @@
 using CourseShop.Services.Catalog.Services;
 using CourseShop.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
 //builder.Services.AddControllers();
-builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+builder.Services.AddControllers(options => {
+
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+    options.Filters.Add(new AuthorizeFilter());
+    
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +26,12 @@ builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 {
     return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
 });
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration.GetValue<string>("IdentityServerURL");
+    options.Audience = "resource_catalog"; 
+    options.RequireHttpsMetadata = false;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
